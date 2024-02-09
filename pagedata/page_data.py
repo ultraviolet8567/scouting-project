86994c8390775fe42c5068530bba044f8953dc35
infernalsquid <arunsiddharth567@gmail.com>
2024-02-09 17:20:00 -0500

    fixed the for loop stuff, dictionaries are fun, next: log entries to postgresql database

--- pagedata/page_data.py
+++ pagedata/page_data.py
@@ -1,21 +1,29 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
 
 competitions = {}
+matches = {}
 
 class comp_data:
-    def __init__(self, team1, team2, date, id):
-        self.team1 = team1
-        self.team2 = team2
+    def __init__(self, caption, date, id):
         self.date = date
         self.id = id
-        self.href = "/landing"
-        self.caption = team1 + " vs " + team2
+        self.href = f'/competitions/{id}'
+        self.caption = caption
         global competitions
         competitions[f'{id}'] = self
+        matches[f'{id}'] = {}
 
-exampleComp = comp_data("Ultraviolet 8567", "Some less cool team XYZA", "11/18/27", 22)
-exampleComp2 = comp_data("Ultraviolet 8567", "Some other team ABCD", "11/11/11", 23)
-print(competitions['22'].date, competitions['22'].caption)
-    
 
-    
+exampleComp = comp_data("Week Zero", "11/18/27", 22)
+exampleComp2 = comp_data("Middle of Nowhere Day 1", "11/11/11", 23)
+
+class match_data:
+    def __init__(self, team1, team2, time, compID, id):
+        self.team1 = team1
+        self.team2 = team2
+        self.time = time
+        self.compID = compID
+        self.id = id
+        matches[f'{compID}'][f'{id}'] = self
