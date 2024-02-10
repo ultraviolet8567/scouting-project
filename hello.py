ee95b83d55498b5e451c2e16bd475d8977a831ac
infernalsquid <arunsiddharth567@gmail.com>
2024-02-10 14:05:56 -0500

    added another page and started unbreaking sql

--- hello.py
+++ hello.py
@@ -1,8 +1,11 @@
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
 from pagedata import page_data
 
 app = Flask(__name__)
+#db = SQLAlchemy(app)
 
 @app.route("/")
 def hello_world():
@@ -14,5 +17,13 @@ def landing():
 
 @app.route("/competitions/<string:competition>")
 def competition_page(competition):
-    return render_template("competition.html", compID=competition)
+    return render_template("competition.html", comp=page_data.competitions[competition], matches=page_data.matches[competition])
 
+#add some approut for post here
+#there should be a column comp id, match id, team #, data, scout
+#def addTeam():
+#    #change this later
+#    teamObj = placeHolderFunction()
+#    for attr in dir(teamObj):
+#        if not attr.startswith("__"):
+            
--- pagedata/page_data.py
+++ pagedata/page_data.py
@@ -19,11 +19,16 @@ class comp_data:
 exampleComp = comp_data("Week Zero", "11/18/27", 22)
 exampleComp2 = comp_data("Middle of Nowhere Day 1", "11/11/11", 23)
 
-class match_data:
+class match_data: #"team1" and "team2" are arrays of 3 strings. team1 is red alliance
     def __init__(self, team1, team2, time, compID, id):
         self.team1 = team1
         self.team2 = team2
         self.time = time
         self.compID = compID
         self.id = id
+        self.datacount = 0
         matches[f'{compID}'][f'{id}'] = self
+
+exampleMatch = match_data([8567, 1111, 2222], [6328, 158, 3333], "12:38", 22, 1)
+exampleMatch2 = match_data([8567, 1111, 2222], [6328, 158, 3333], "16:16", 23, 1)
+exampleMatch3 = match_data([8567, 12, 1], [4000, 4500, 4554], "11:11", 22, 2)
